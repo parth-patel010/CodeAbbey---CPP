c846266e980cd85d401cd828cb0af5d8a3d282be
Parth_Patel__. <parthp12321@gmail.com>
2025-06-09 00:33:38 +0530

    12

---
+++ 12.Triangles.cpp
@@ -0,0 +1,39 @@
+// CodeAbby Problem 10 Vowel Count using C++
+/*
+====Expected Output====
+data:
+2
+3 4 5
+1 2 4
+
+answer:
+1 0
+*/
+
+//Note:- Check Condition A + B > C && A + C > B && B + C > A
+
+#include<iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+    int arr[n * 3];
+
+    for (int i = 0; i < n * 3; i++) {
+        std::cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        int A = arr[i * 3];
+        int B = arr[i * 3 + 1];
+        int C = arr[i * 3 + 2];
+
+        if (A + B > C && A + C > B && B + C > A) {
+            std::cout << 1 << " ";
+        } else {
+            std::cout << 0 << " ";
+        }
+    }
+
+    return 0;
+}
